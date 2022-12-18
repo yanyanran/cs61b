e9a09c90b00e174b2cdb91ffe2faf24c308a66ad
yanyanran <yanran66666@qq.com>
2022-12-18 23:41:17 +0800

    task5完成

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,6 +1,19 @@
 /**
  * 一个用于 off-by-N 比较器的类
  * */
-public class OffByN {
+public class OffByN implements CharacterComparator {
+    private int N;
 
+    // 构造函数
+    public OffByN(int n){
+        N = n;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        if (Math.abs(x - y) == N) {
+            return true;
+        }
+        return false;
+    }
 }
