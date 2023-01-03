7c654de40c8821d0732924bbcea0ed5915a2891b
yanyanran <yanran66666@qq.com>
2023-01-03 22:54:56 +0800

    hw2建立框架

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,43 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
+/**
+ * 如果任何参数open(), isOpen(), orisFull()超出其规定范围，则抛出一个java.lang.IndexOutOfBoundsException。
+ * N≤0 构造函数应该抛出一个java.lang.IllegalArgumentException
+ * */
 public class Percolation {
+    // 创建N乘N网格，最初阻止所有站点
+    public Percolation(int N) {
 
+    }
+
+    // 如果站点尚未打开，请打开该站点（行、列）
+    public void open(int row, int col) {
+
+    }
+
+    // 站点（行、列）是否打开？
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    // 站点（行、列）满了吗？
+    public boolean isFull(int row, int col) {
+
+    }
+
+    // 开放站点数量
+    public int numberOfOpenSites() {
+
+    }
+
+    // 系统是否渗透？
+    public boolean percolates() {
+
+    }
+
+    // 用于单元测试（不需要，但请将其保存在此处以供签名者使用）
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,30 @@
 package hw2;
-
+/**
+ * 如果N≤0 或T≤0 ，构造函数应该抛出一个java.lang.IllegalArgumentException
+ * */
 public class PercolationStats {
+    // 在N乘N网格上执行T个独立实验
+    public PercolationStats(int N, int T, PercolationFactory pf) {
 
+    }
+
+    // 逾渗阈值的样本均值
+    public double mean() {
+
+    }
+
+    // 渗滤阈值的样本标准差
+    public double stddev() {
+
+    }
+
+    // 95%置信区间的低终点
+    public double confidenceLow() {
+
+    }
+
+    // 95%置信区间的高终点
+    public double confidenceHigh() {
+
+    }
 }
