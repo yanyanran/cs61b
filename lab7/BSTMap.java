34e8bca7eadfae9b04a31674e811026fd5705caf
yanyanran <yanran66666@qq.com>
2023-01-05 21:07:27 +0800

    lab7框架构建

---
+++ lab7/BSTMap.java
@@ -0,0 +1,56 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    /**
+     * 按增加键的顺序打印出您的BSTMap
+     * */
+    public void printInOrder() {
+
+    }
+}
